3d950d7094ab68f7b31e80c14d68af41e4f73d92
benStre <strehle.benedikt@gmail.com>
2022-11-01 15:40:36 +0100

    gitignore

--- runner/constants.js
+++
@@ -1,9 +0,0 @@
-export const VERSION = '1.0-beta';
-export const SUPPORTED_EXTENSIONS = [
-    '.test.js',
-    '.test.dx'
-];
-export const SUPPORTED_REPORT_TYPES = [
-    'junit'
-];
-export const BOX_WIDTH = 95;
