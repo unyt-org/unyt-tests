61d1d4313650c1f8352fb8851b3ac164e769d932
benStre <strehle.benedikt@gmail.com>
2023-08-19 14:44:06 +0200

    remove rust target

--- test_examples/rust/target/debug/deps/rust-1f4e8e0968e2a102.d
+++
@@ -1,5 +0,0 @@
-/home/unyt/networks/main/dockers/CDN/data/lib/unyt_tests/test_examples/rust/target/debug/deps/rust-1f4e8e0968e2a102: src/main.rs
-
-/home/unyt/networks/main/dockers/CDN/data/lib/unyt_tests/test_examples/rust/target/debug/deps/rust-1f4e8e0968e2a102.d: src/main.rs
-
-src/main.rs:
